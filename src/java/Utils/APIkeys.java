58bb9113fa0ae8f75f56f260357b0d936ec7bf56
Clement Levallois <clementlevallois@gmail.com>
2013-03-31 20:50:59 +0200

    delete Utils

--- src/java/Utils/APIkeys.java
+++ src/java/Utils/APIkeys.java
@@ -16,4 +16,22 @@ public class APIkeys {
     public static String getScopusRingAPIkey() {
         return "e0bf3f89dbe4c27aea89acdcfd85d5c3";
     }
+    
+    public static String getMongoHQAPIkey() {
+        return "0FwGVJmwy8ouyeZ1z6p7xQ";
+    }
+    
+    public static String getMongoHQPass() {
+        return "testpass";
+    }
+    
+    public static String getTwitterConsumerSecret() {
+        return "VVsf4qT0DCBeLODRDnBlhwxrRG6KLm0TT2wiK2Q";
+    }
+    
+    public static String getTwitterAccessTokenSecret() {
+        return "P7l7SmHiZyE11tNfsQ17fdSzJ7sUpMJAliundncpaA";
+    }
+    
+    
 }
